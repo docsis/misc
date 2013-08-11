8154e3ad2b6e16f089bc7e05bd48b281614dbe15
wuyajun <yajwu@vb-12.(none)>
2013-08-11 11:26:04 +0800

    add

--- link.sh
+++ link.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-rm ~/.vimrc
-rm -rf ~/.vim
+[ -f ~/.vimrc ] &&  mv ~/.vimrc ~/.vimrc.bak
+[ -d ~/.vim ] && mv ~/.vim ~/.vim.bak
 ln -s  `pwd`/.vimrc ~/.vimrc
 ln -s  `pwd`/.vim ~/.vim
 
 cp .bash_aliases ~/
+
+[ -d ~/bin ] || mkdir ~/bin
+
+for p in bin/*
+do
+    pname=`basename $p`
+    [ -L ~/bin/$pname ] && rm ~/bin/$pname 
+    ln -s `pwd`/$p ~/bin/$pname
+done
+
