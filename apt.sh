b7cb424a08bd7c065b6fbc500a983e73b247c270
yajwu <2308524@qq.com>
2017-11-14 23:15:50 +0800

    add apt-get install list

---
+++ apt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get install tree
+apt-get install awk
+apt-get install gawk
+apt-get install zlib1g-dev
+apt-get install curl
+apt-get install tcsh
+apt-get install tclsh
+apt-get install ctags cscope
+apt-get install vim
+apt-get install git
+
