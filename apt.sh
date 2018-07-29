794f6b3f0fa926fa23f06f86cabc2e91387c3be4
root <root@MyServer.(none)>
2018-07-29 08:40:50 -0400

    change

--- apt.sh
+++ apt.sh
@@ -1,20 +1,26 @@
-apt-get install ctags cscope
-apt-get install ctags cscope -y
-apt-get install fonts-wqy-zenhei -y
-apt-get install iceweasel
-apt-get install lxterminal -y
+#!/bin/bash
+
+apt-get install ctags cscope vim -y
+apt-get install sudo aptitude -y
 apt-get install python-openssl
 apt-get install python-pip
 apt-get install python-requests
 apt-get install python-urllib3
+apt-get install gcc -y
+
+gui () {
+apt-get install fonts-wqy-zenhei -y
+apt-get install iceweasel
+apt-get install lxterminal -y
 apt-get install tightvncserver
 apt-get install xfce
 apt-get install xfonts-wqy -y
 apt-get install xorg xdm xfce4
 apt-get install xubuntu-desktop
-
-
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install pyopenssl ndg-httpsclient pyasn1
-pip install requests[security] 
+pip install requests
+}
+
+
