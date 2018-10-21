13c9c58a683d750af2b2eb85b6fe0ce449db8d78
yajun <2308524@qq.com>
2018-10-21 17:29:21 +0800

    change apt.sh

--- apt.sh
+++ apt.sh
@@ -2,11 +2,9 @@
 
 apt-get install ctags cscope vim -y
 apt-get install sudo aptitude -y
-apt-get install python-openssl
-apt-get install python-pip
-apt-get install python-requests
-apt-get install python-urllib3
 apt-get install gcc -y
+
+update-alternatives --config editor
 
 gui () {
 apt-get install fonts-wqy-zenhei -y
@@ -21,6 +19,10 @@ wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install pyopenssl ndg-httpsclient pyasn1
 pip install requests
+apt-get install python-openssl
+apt-get install python-pip
+apt-get install python-requests
+apt-get install python-urllib3
 }
 
 
